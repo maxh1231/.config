ae223e9c3df039fdad29af7147577f662cfc21fe
max <maxhumpherys@gmail.com>
2025-05-29 23:08:21 -0600

    aliases

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 
 ln -sf "$HOME/.config/zsh/.zshrc" "$HOME/.zshrc"
 ln -sf "$HOME/.config/zsh/.zprofile" "$HOME/.zprofile"
+ln -sf "$HOME/.config/zsh/aliases.zsh" "$HOME/.oh-my-zsh/custom/aliases.zsh"
+
