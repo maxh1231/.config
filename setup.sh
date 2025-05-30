2b5249c34f454ca8d20582afeb71030e3fbf204b
max <maxhumpherys@gmail.com>
2025-05-29 23:57:49 -0600

    fzf, vsc, bat, ghostty

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ ln -sf "$HOME/.config/zsh/.zprofile" "$HOME/.zprofile"
 ln -sf "$HOME/.config/zsh/aliases.zsh" "$HOME/.oh-my-zsh/custom/aliases.zsh"
 ln -sf "$HOME/.config/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
 
+ln -sf "$HOME/.config/.vscode/extensions/extensions.json" "$HOME/.vscode/extensions/extensions.json"
+
+ln -sf "$HOME/.config/.vscode/User/settings.json" "$HOME/.vscode/User/settings.json"
+
