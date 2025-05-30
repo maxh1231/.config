50db0d82190800b3d1e42e48c77092db6ad19f3b
max <maxhumpherys@gmail.com>
2025-05-29 23:17:23 -0600

    powerlevel10k

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 ln -sf "$HOME/.config/zsh/.zshrc" "$HOME/.zshrc"
 ln -sf "$HOME/.config/zsh/.zprofile" "$HOME/.zprofile"
 ln -sf "$HOME/.config/zsh/aliases.zsh" "$HOME/.oh-my-zsh/custom/aliases.zsh"
+ln -sf "$HOME/.config/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
 
