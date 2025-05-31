43366636d5aa834fef9777a4b610923c04bd8860
max <maxhumpherys@gmail.com>
2025-05-31 17:56:42 -0600

    tmux

--- setup.sh
+++ setup.sh
@@ -9,4 +9,30 @@ ln -sf "$HOME/.config/.vscode/extensions/extensions.json" "$HOME/.vscode/extensi
 
 ln -sf "$HOME/.config/.vscode/User/settings.json" "$HOME/.vscode/User/settings.json"
 
+ln -sf "$HOME/.config/tmux/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$HOME/.config/tmux/.tmux-cht-languages" "$HOME/.tmux-cht-languages"
+ln -sf "$HOME/.config/tmux/.tmux-cht-command" "$HOME/.tmux-cht-command"
+
+link_script() {
+    local src="$HOME/.config/tmux/$1"
+    local target="$HOME/.local/bin/$1"
+
+    if [ ! -e "$src" ]; then
+        echo "$1 not found"
+        return
+    fi
+
+    chmod +x "$src"
+
+    if [ -e "$target" ] || [ -L "$target" ]; then
+        echo "$target already exists."
+    else 
+        ln -s "$src" "$target"
+        echo "Symlink created at $target"
+    fi
+}
+
+link_script "tmux-cht.sh"
+link_script "tmux-sessionizer"
+
 cp -L "$HOME/.config/.gitconfig" "$HOME/.gitconfig"
