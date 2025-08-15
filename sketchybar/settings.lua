2be9144ff8bc73791b6c437b18438cb19287bf54
max <max@humpherys.dev>
2025-08-14 22:34:21 -0600

    sketchybar lua integration

---
+++ sketchybar/settings.lua
@@ -0,0 +1,28 @@
+return {
+    paddings = 3,
+    group_paddings = 5,
+    icons = "sf-symbols",
+    font = {
+        text = "SF Pro",
+        numbers = "SF Mono",
+        style_map = {
+            ["Regular"] = "Regular",
+            ["Semibold"] = "Semibold",
+            ["Bold"] = "Bold",
+            ["Heavy"] = "Heavy",
+            ["Black"] = "Black",
+        }
+    }
+    -- Alternatively, this is a font config for JetBrainsMono Nerd Font
+    -- font = {
+    --   text = "JetBrainsMono Nerd Font", -- Used for text
+    --   numbers = "JetBrainsMono Nerd Font", -- Used for numbers
+    --   style_map = {
+    --     ["Regular"] = "Regular",
+    --     ["Semibold"] = "Medium",
+    --     ["Bold"] = "SemiBold",
+    --     ["Heavy"] = "Bold",
+    --     ["Black"] = "ExtraBold",
+    --   },
+    -- },
+}
